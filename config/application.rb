6e9d225dff64c4389d6782b905bd7894d0eebafc
Junichi Ito <jit@sonicgarden.jp>
2014-10-05 07:11:03 +0900

    Update generator options.

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,16 @@ module DynamicDossierSandbox
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: true,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false,
-        controller_specs: false,
-        request_specs: false
+                       fixtures: false,
+                       view_specs: false,
+                       controller_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false
+      g.assets false
+      g.helper false
+      g.javascripts false
+      g.stylesheets false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
