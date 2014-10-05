4a61a4729c45ad27fd1456f972ee78e9fd67cfac
Junichi Ito <jit@sonicgarden.jp>
2014-10-05 15:26:40 +0900

    Use dynamic params.

--- app/reports/dynamic_dossier_report.rb
+++ app/reports/dynamic_dossier_report.rb
@@ -7,8 +7,22 @@ class DynamicDossierReport < Dossier::Report
     dynamic_report.name
   end
 
+  def report_params
+    dynamic_report.report_params
+  end
+
   def dynamic_report
     report_name = options['report_name']
-    @dynamic_report ||= DynamicReport.find_by_name(report_name)
+    if @dynamic_report.blank?
+      @dynamic_report = DynamicReport.find_by_name(report_name)
+      @dynamic_report.report_params.each do |param|
+        singleton_class.class_eval do
+          define_method param.name do
+            options[param.name]
+          end
+        end
+      end
+    end
+    @dynamic_report
   end
 end
--- app/reports/user_age_distribution_report.rb
+++ app/reports/user_age_distribution_report.rb
@@ -3,10 +3,16 @@ class UserAgeDistributionReport < Dossier::Report
     <<-SQL
 select strftime('%Y', birthday) as birth_year, count(*) as user_count
 from users
+where
+annual_income > :annual_income_greater_than
 group by
 birth_year
 order by
 birth_year
     SQL
+  end
+
+  def annual_income_greater_than
+    options[:annual_income_greater_than].to_i
   end
 end
