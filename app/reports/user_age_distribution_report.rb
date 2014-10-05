bb723975ff2c7bdc79bfa67d59f1ad381e4f12c2
Junichi Ito <jit@sonicgarden.jp>
2014-10-05 09:40:34 +0900

    Refine SQL.

--- app/reports/user_age_distribution_report.rb
+++ app/reports/user_age_distribution_report.rb
@@ -1,7 +1,12 @@
 class UserAgeDistributionReport < Dossier::Report
   def sql
     <<-SQL
-select birthday from users
+select strftime('%Y', birthday) as birth_year, count(*) as user_count
+from users
+group by
+birth_year
+order by
+birth_year
     SQL
   end
 end
